34f2fb529ba76e30f3c2d49695393304af357162
Salome Ranson <salomeranson@gmail.com>
2021-08-14 15:24:19 -0400

    implemented a Priority Queue with Min Binary Heap

--- interviewPrep.js
+++ interviewPrep.js
@@ -1910,8 +1910,6 @@
 //            index = swap;
 //         }
 //     }
-
-
 // }
 
 // let heap = new MaxBinaryHeap();
@@ -1929,3 +1927,113 @@
 // // Before extractMax()              [55, 39, 41, 18, 27, 12, 33]
 // heap.extractMax();                              
 // // After extractMax()               [41, 39, 33, 18, 27, 12]
+
+// ===================================================================================================================================================================================
+
+
+
+
+// Priority Queue:
+// What is a priroty queue?
+
+// An abstract concept/ data structure where each element has a priority.
+// Elements with higher priorities are served before elements with lower priorities
+// Often implemented with Binary Heaps
+
+
+
+//------------------------------------------- Min Binary Heap Priority Queue Implementation --------------------------
+// Lower number means higher priority
+
+
+// class PriorityQueue {
+//     constructor() {
+//         this.values = [];
+//     }
+
+//     enqueue(value, priority) {
+//         let newNode = new Node(value, priority)
+//         this.values.push(newNode)
+//         this.bubbleUp();
+//     }
+
+//     bubbleUp() {
+//         let index = this.values.length - 1;
+//         let element = this.values[index]
+//         while(index > 0) {
+//             let parentIndex = Math.floor((index -1) / 2);           
+//             let parent = this.values[parentIndex];        
+//             if(element.priority >= parent.priority) break;          
+//                 this.values[parentIndex] = element;     
+//                 this.values[index] = parent;      
+//                 index = parentIndex;
+//         }
+//     }
+
+//     // Remove the root from Max Heap
+//     dequeue() {
+//         const min = this.values[0];       
+//         const end = this.values.pop();           
+//         if(this.values.length > 0)  {
+//             this.values[0] = end;           
+//             this.sinkDown();          
+//         }
+//         return min;         
+//     }
+
+//     sinkDown() {
+//         let index = 0;                                              
+//         const length = this.values.length;
+//         const element = this.values[0];                         
+//         while(true) {
+//            let leftChildIndex = 2 * index + 1;
+//            let rightChildindex = 2 * index + 2;
+//            let leftChild, rightChild;
+//            let swap = null;
+           
+//            if(leftChildIndex < length) {
+//                leftChild = this.values[leftChildIndex];
+//                if(leftChild.priority < element.priority) {
+//                    swap = leftChildIndex;
+//                }
+//            }
+//            if(rightChildindex < length) {
+//                rightChild = this.values[rightChildindex];
+//                if(
+//                    (swap === null && rightChild.priority < element.priority) ||
+//                    (swap !== null && rightChild.priority < leftChild.priority)
+//                ) {
+//                    swap = rightChildindex;
+//                }
+//            }
+//            if(swap === null) break;
+//            this.values[index] = this.values[swap];
+//            this.values[swap] = element;
+//            index = swap;
+//         }
+//     }
+// }
+
+// class Node {
+//     constructor(value, priority) {
+//         this.value = value;
+//         this.priority = priority;
+//     }
+// }
+
+
+// let ER = new PriorityQueue();
+// console.log(ER);
+
+// // Enqueue (Add to queue)
+// ER.enqueue("common cold", 5);
+// ER.enqueue("gunshot wound", 1);
+// ER.enqueue("high fever", 4);
+// ER.enqueue("broken arm", 2);
+// ER.enqueue("glass in foot", 3);
+
+// // Dequeue
+// // ER.dequeue();
+
+
+

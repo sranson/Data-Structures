9e1583eb13ad3559d36ff87d227a486ea3a04ae9
Salome Ranson <salomeranson@gmail.com>
2021-08-15 12:23:56 -0400

    added depth first graph traversal recursively

--- interviewPrep.js
+++ interviewPrep.js
@@ -2207,7 +2207,6 @@
 //*
 // ===================================================================================================================================================================================
 
-
 // Section 26:
 // Graphs
 
@@ -2342,6 +2341,124 @@
 
 // console.log(myGraph);
 
+// ===================================================================================================================================================================================
+//*
+//*
+//*
+//*
+//*
+//*
+//*
+//*
+//*                                                                                NEXT SECTION
+//*
+//*
+//*
+//*
+//*
+//*
+//*
+// ===================================================================================================================================================================================
+
+// Section 27:
+// Graph Traversal
+
+
+// Graph Traversal Uses:
+//======================
+// Peer to peer networking
+// Web crawlers
+// Finding "closest" matches/recommendations
+// Shortest path problems
+        // GPS Navigation
+        // Solving mazes
+        // AI (shortest path to win the game)
+
+
+
+// Depth First Graph Traversal:
+//=============================
+// Follow a branch down/follow the vertex's neighbors down
+
+
+// Breadth First Graph Traversal:
+//===============================
+// Visit neighbors at current depth FIRST
+
+
+class Graph {
+    constructor() {
+       this.adjacencyList = {}; 
+    }
+
+    addVertex(vertex) {
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];            
+    }
+
+    // Undirected graph (Two-way connection)
+    addEdge(v1, v2) {
+        if(!this.adjacencyList[v1] || !this.adjacencyList[v2]) return undefined;
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);                                                    
+    }
+
+    removeEdge(v1, v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(vertex => vertex !== v2)
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(vertex => vertex !== v1)
+    }
+
+    removeVertex(vertex) {
+        while(this.adjacencyList[vertex].length) {                                          
+            const adjacentVertex = this.adjacencyList[vertex].pop()                         
+            this.removeEdge(vertex, adjacentVertex);                                        
+        }
+        delete this.adjacencyList[vertex];                                                  
+    }
+
+    depthFirstRecursive(start) {
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        (function dfs(vertex) {
+            if(!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]) {
+                    return dfs(neighbor); 
+                }
+            });
+        })(start)
+
+        return result;
+    }
+
+
+}
+
+
+let g = new Graph();
+
+console.log(g);
+
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B", "D")
+g.addEdge("C", "E")
+g.addEdge("D", "E")
+g.addEdge("D", "F")
+g.addEdge("E", "F")
+
+
+console.log(g.depthFirstRecursive("A"));
 
 
 
